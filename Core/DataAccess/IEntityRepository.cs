20f5b7ce3ad1dea9756268340191a9e9f769094d
ted <started2122@gmail.com>
2021-02-14 15:40:36 +0300

    9.gün 1.bölüm Code Refactoring Evrensel Core katmanı eklendi. IEntityRepository, IEntity Core'a taşındı.

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,10 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {   // class : must referance type  not int,bool
     // IEntity : IEntity olabilir veya IEntity implement eden bir nesne olabilir
     public interface IEntityRepository<T> where T : class, IEntity, new()
